9717d2c1bd58cdd6ca9246b18b9a94a3071bba12
김민지 <kimminji@gimminjiui-MacBookAir.local>
2025-06-10 17:47:43 +0900

    사이드바 수정

--- pages/7_📕_데이터분석_(2)_분석주제선택.py
+++ pages/7_📕_데이터분석_(2)_분석주제선택.py
@@ -4,7 +4,7 @@ st.set_page_config(
     page_icon="📕",
     layout="centered"
 )
-# 🔒 자동 생성된 사이드바 메뉴 숨기기
+
 hide_default_sidebar = """
     <style>
     [data-testid="stSidebarNav"] {
@@ -13,7 +13,34 @@ hide_default_sidebar = """
     </style>
 """
 st.markdown(hide_default_sidebar, unsafe_allow_html=True)
+
 st.title("📕 (2) 분석 주제 선택")
+
+with st.sidebar:
+    # 🏠 홈으로
+    st.page_link("app.py", label="HOME", icon="🏠")
+    st.markdown("---")
+
+    st.markdown("## 🤖 경사하강법")
+    st.page_link("pages/1_📘_경사하강법_(1)_최적화란.py", label="(1) 최적화란?")
+    st.page_link("pages/2_📘_경사하강법_(2)_학습률이란.py", label="(2) 학습률이란?")
+    st.page_link("pages/3_📘_경사하강법_(3)_반복횟수란.py", label="(3) 반복횟수란?")
+
+    st.markdown("---")
+
+    st.markdown("## 💻 시뮬레이션")
+    st.page_link("pages/4_📒_시뮬레이션_(1)_학습률_실험.py", label="(1) 학습률 실험")
+    st.page_link("pages/5_📒_시뮬레이션_(2)_반복횟수_실험.py", label="(2) 반복횟수 실험")
+
+    st.markdown("---")
+
+    st.markdown("## 📊 데이터분석")
+    st.page_link("pages/6_📕_데이터분석_(1)_기본정보입력.py", label="(1) 기본 정보 입력")
+    st.page_link("pages/7_📕_데이터분석_(2)_분석주제선택.py", label="(2) 분석 주제 선택")
+    st.page_link("pages/8_📕_데이터분석_(3)_데이터입력.py", label="(3) 데이터 입력")
+    st.page_link("pages/9_📕_데이터분석_(4)_예측실행.py", label="(4) 예측 실행")
+    st.page_link("pages/10_📕_데이터분석_(5)_예측해석.py", label="(5) 예측 해석")
+    st.page_link("pages/11_📕_데이터분석_(6)_요약결과.py", label="(6) 요약 결과")
 
 # 1단계에서 입력하지 않을 경우 경고 멘트 
 if "name" not in st.session_state:
@@ -43,29 +70,3 @@ if "subject" in st.session_state:
     with col3:
         if st.button("➡️ 다음"):
             st.switch_page("pages/8_📕_데이터분석_(3)_데이터입력.py")
-
-with st.sidebar:
-    # 🏠 홈으로
-    st.page_link("app.py", label="HOME", icon="🏠")
-    st.markdown("---")
-
-    st.markdown("## 🤖 경사하강법")
-    st.page_link("pages/1_📘_경사하강법_(1)_최적화란.py", label="(1) 최적화란?")
-    st.page_link("pages/2_📘_경사하강법_(2)_학습률이란.py", label="(2) 학습률이란?")
-    st.page_link("pages/3_📘_경사하강법_(3)_반복횟수란.py", label="(3) 반복횟수란?")
-
-    st.markdown("---")
-
-    st.markdown("## 💻 시뮬레이션")
-    st.page_link("pages/4_📒_시뮬레이션_(1)_학습률_실험.py", label="(1) 학습률 실험")
-    st.page_link("pages/5_📒_시뮬레이션_(2)_반복횟수_실험.py", label="(2) 반복횟수 실험")
-
-    st.markdown("---")
-
-    st.markdown("## 📊 데이터분석")
-    st.page_link("pages/6_📕_데이터분석_(1)_기본정보입력.py", label="(1) 기본 정보 입력")
-    st.page_link("pages/7_📕_데이터분석_(2)_분석주제선택.py", label="(2) 분석 주제 선택")
-    st.page_link("pages/8_📕_데이터분석_(3)_데이터입력.py", label="(3) 데이터 입력")
-    st.page_link("pages/9_📕_데이터분석_(4)_예측실행.py", label="(4) 예측 실행")
-    st.page_link("pages/10_📕_데이터분석_(5)_예측해석.py", label="(5) 예측 해석")
-    st.page_link("pages/11_📕_데이터분석_(6)_요약결과.py", label="(6) 요약 결과")
